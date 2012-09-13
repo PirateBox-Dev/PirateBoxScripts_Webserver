f23f3f4f1f1010e10fa78adef0782a7705ab2c69
Matthias Strubel <matthias.strubel@aod-rpg.de>
2012-09-13 07:19:16 +0200

    severall fixes aroung shoutbox modules  #16

--- piratebox/piratebox/www/cgi-bin/psogen.py
+++ piratebox/piratebox/www/cgi-bin/psogen.py
@@ -19,25 +19,21 @@ def generate_html(content):
     htmlfile = open( htmlfilename , 'w' )
     htmlfile.write( "<html><head><meta name='GENERATOR' content='PyShoutOut'><title>Shout-Out Data</title><style type='text/css'>" )
     htmlfile.write( "<style>" + stl   + "</style></head><body>"  )
-    htmlfile.write( ( content )
-    htmlfile.write ( "</body></html>" )
+    htmlfile.write( content )
+    htmlfile.write( "</body></html>" )
     htmlfile.close()
 
-def generate_html_from_file()
-    datafile = open(datafilename, 'r+')
-    old = datafile.read()
-    datafile.truncate(0)
-    datafile.close()
-    generate_html( read_data_file()   )
+def generate_html_from_file():
+    old =  read_data_file() 
+    generate_html( old   )
 
-def read_data_file()
-    datafile = open(datafilename, 'r+')
+def read_data_file():
+    datafile = open(datafilename, 'r')
     old = datafile.read()
-    datafile.truncate(0)
     datafile.close()
     return old
 
-def process_form( name , indata , color )
+def process_form( name , indata , color ):
     old = read_data_file()
     datapass = re.sub("<", "&lt;", indata)
     data = re.sub(">", "&gt;", datapass)
@@ -46,18 +42,18 @@ def process_form( name , indata , color )
 
     finalcontent = "<date>" + curdate.strftime("%H:%M:%S") + "</date>&nbsp;&nbsp;<name>" + name + ":</name>&nbsp;&nbsp;&nbsp;<data class='" + color + "'>" + data + "</data><br>\n" + old 
     datafile = open(datafilename, 'r+')
-    datafile.write(finalecontent)
+    datafile.write(finalcontent)
+    datafile.truncate(0)
     datafile.close()
     generate_html(finalcontent)
 
 
-
 if __name__ == "__main__":
   import sys
-  if sys.argv[1] == "input" :
-     process_form (  sys.argv[2] ,  sys.argv[3] ,  sys.argv[4] )
+  if sys.argv.count("input") >= 1 :
+     process_form(  sys.argv[2] ,  sys.argv[3] ,  sys.argv[4] )
      print "Entered Text."
-  else
-     generate_html_from_file (   )
-     print "Generated HTML File.\n"
+     
+  generate_html_from_file()
+  print "Generated HTML File."
 
