5ec059384cdb5d6ba135c677d27193173323c54d
Matthias Strubel <matthias.strubel@aod-rpg.de>
2012-10-05 21:54:01 +0200

    First implementation of timesave #18

---
+++ piratebox/piratebox/bin/timesave.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+#  This script enables a sort of timerescue System
+#  for Systems without a Realtime Clock
+#  like TP-Link MR3020 , RaspberryPI
+#
+#  It does not reflect the real time, but 
+#  gives a sort of stability to complete standalone 
+#  systems.
+#
+#  Licenced under GPL-2  @ 2012
+#    Matthias Strubel    matthias.strubel@aod-rgp.de
+
+
+# Load configfile
+
+if [ -z  $1 ] || [ -z $2 ] ; then
+  echo "Set up a crontab entry for regulary saving the time"
+  echo "Usage $0 <path to piratebox.conf> <step>"
+  echo "    Valid steps are:"
+  echo "       install    - installs the needed parts into crontab"
+  echo "       save       - saves time into file"
+  echo "       recover    - recovers the time from a file"
+
+  exit 1
+fi
+
+TIMESAVE="$PIRATEBOX_FOLDER/timesave_file"
+
+if [ "$2" = "install" ] ; then
+    crontab -l   >  $PIRATEBOX_FOLDER/tmp/crontab 2> /dev/null
+    echo "#--- Crontab for PirateBox-Timesave" >>  $PIRATEBOX_FOLDER/tmp/crontab
+    echo " */5 * * * *   $PIRATEBOX_FOLDER/bin/$0 $PIRATEBOX_FOLDER/conf/piratebox.conf save "  >> $PIRATEBOX_FOLDER/tmp/crontab
+    crontab $PIRATEBOX_FOLDER/tmp/crontab
+
+    touch $TIMESAVE
+    chmod a+rw $TIMESAVE
+
+    if [  "$OPENWRT" = "yes" ] ; then
+	/etc/init.d/cron enable
+	/etc/init.d/cron start
+    else 
+       echo "Remember to have cron active..."
+    fi
+    #Save the current time
+    $0 $1 "save"
+    exit 0
+fi
+
+if [ "$2" = "save" ] ; then
+    #Save Datetime in a recoverable format...
+    date +%C%g%m%d%H%M  > $TIMESAVE
+    exit 0
+fi
+
+if [ "$2" = "recover" ] ; then
+    date  `cat $TIMESAVE `
+    [ "$?" != "0" ] &&  echo "error in recovering time" && exit 255
+    echo "Time recovered"
+    exit 0
+fi
+
