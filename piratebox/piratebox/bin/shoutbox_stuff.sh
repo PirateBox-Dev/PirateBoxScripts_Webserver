6715254be1a15ce5ca2593f4d58a2dce5b3ef852
Matthias Strubel <matthias.strubel@aod-rpg.de>
2012-11-27 20:41:22 +0100

    fixed some shoutbox issues & tidied up a few things

--- piratebox/piratebox/bin/shoutbox_stuff.sh
+++ piratebox/piratebox/bin/shoutbox_stuff.sh
@@ -19,7 +19,19 @@ cd python_lib
 export SHOUTBOX_CHATFILE=$CHATFILE
 export SHOUTBOX_GEN_HTMLFILE=$GEN_CHATFILE
 
+
+#Writing init-message and reset chat..
+if [ "$RESET_CHAT"  = "yes" ] ; then
+   echo $CHATMSG > $CHATFILE
+fi
+
+#Generate content file
 python psogen.py generate
+
+#Set correct permissions
+chown $LIGHTTPD_USER:$LIGHTTPD_GROUP $SHOUTBOX_CHATFILE
+chown $LIGHTTPD_USER:$LIGHTTPD_GROUP $SHOUTBOX_GEN_HTMLFILE
+
 
 if [ "$GLOBAL_CHAT" = "yes" ] ; then
      export SHOUTBOX_BROADCAST_DESTINATIONS=$GLOBAL_DEST
