fdedbaa7033f10b338b3336d649012d63ccd457c
Matthias Strubel <matthias.strubel@aod-rpg.de>
2012-12-26 11:20:16 +0100

    Start/stop global shoutbox service via init

---
+++ piratebox/piratebox/bin/shoutbox_daemon.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Starts deamon for global shoutbox service
+#  requires  piratebox.conf as first parameter
+#
+#  Matthias Strubel - 2012
+#  Licenced with GPL
+
+. $1
+
+cd $PIRATEBOX_FOLDER
+cd python_lib
+
+export SHOUTBOX_CHATFILE=$CHATFILE
+export SHOUTBOX_GEN_HTMLFILE=$GEN_CHATFILE
+
+
+if [ "$GLOBAL_CHAT" = "yes" ] ; then
+     export SHOUTBOX_BROADCAST_DESTINATIONS="$GLOBAL_DEST"
+fi
+
+exec python discover.py
