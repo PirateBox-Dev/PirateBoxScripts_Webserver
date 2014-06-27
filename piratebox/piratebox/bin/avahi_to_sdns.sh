81871fe7b3657392a22696432f68eabfe7895d27
Matthias Strubel <matthias.strubel@aod-rpg.de>
2014-06-27 22:01:14 +0200

    Draft for mapping avahi browse results to a hosts file

---
+++ piratebox/piratebox/bin/avahi_to_sdns.sh
@@ -0,0 +1,23 @@
+PIRATEBOX_HOSTS_MESH=.../hosts_mesh
+DNSMASQ_PID=....
+
+SLEEPTIME=$((60 * 5 ))  # Sleep for 5 Minutes before doing a refresh
+
+
+while true; do
+
+	## Browse Avahi discover results and format them into a hosts file
+	avahi-browse -f rtp_http._tcp | grep "=" > /tmp/avahi.browse
+	grep \= /tmp/avahi-browse | \
+		sed -e 's|\.local||g' \
+		   -e 's|_|\.|g' | \
+		awk 'BEGIN { FS=";" } { print $8 " " $7; }' > /tmp/avahi.browse.hosts
+
+
+	echo "# Updated " `date` >> /tmp/avahi.browse.hosts
+	cp /tmp/avahi.browse.hosts $PIRATEBOX_HOSTS_MESH
+
+	# Send SIGHUP to dnsmasq for refreshing its cache
+	kill -1 `cat $DNSMASQ_PID`
+	sleep $SLEEPTIME
+done
