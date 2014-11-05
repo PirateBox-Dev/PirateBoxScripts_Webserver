804627901e0ec9a057a3dad354aaaa08868e6e7e
Matthias Strubel <matthias.strubel@aod-rpg.de>
2014-11-05 18:53:56 +0100

    Feature: board-autoconf.sh for OpenWrt available Made the board un-useable, if it is not setup.
     Conflicts:
    	CHANGELOG

--- piratebox/piratebox/bin/board-autoconf.sh
+++ piratebox/piratebox/bin/board-autoconf.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bh
 
 if [[ ! -d /opt/piratebox/share/board ]]; then
     echo "You have to install the imageboard first!"
@@ -12,4 +12,7 @@ else
 
     TEMPRAND=$(< /dev/urandom tr -dc A-Za-z0-9_ | head -c128)
     sed -i "s|xyzSECRETCODEzyx|$TEMPRAND|g" /opt/piratebox/share/board/config.pl
+
+    sed -i "s|#use constant ADMIN_PASS|use constant ADMIN_PASS|" /opt/piratebox/share/board/config.pl
+    sed -i "s|#use constant SECRET|use constant SECRET|" /opt/piratebox/share/board/config.pl
 fi
