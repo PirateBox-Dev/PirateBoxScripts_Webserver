00e220ae2a439f30ba5a8836aaf8265874d48fcd
Matthias Strubel <matthias.strubel@aod-rpg.de>
2015-02-09 15:26:59 +0100

    Fixes in hosts generation

--- piratebox/piratebox/bin/avahi_to_sdns.sh
+++ piratebox/piratebox/bin/avahi_to_sdns.sh
@@ -1,21 +1,25 @@
 PIRATEBOX_HOSTS_MESH=/opt/piratebox/conf/hosts_mesh
+AVAHI_CACHE_FILE=/tmp/avahi.browse
+AVAHI_GEN_OUTPUT="${AVAHI_CACHE_FILE}".hosts
+
 DNSMASQ_PID=/var/run/piratebox_dnsmasq.pid
 
 SLEEPTIME=$((60 * 5 ))  # Sleep for 5 Minutes before doing a refresh
 
+PROTOCOLL="_http._tcp"
 
 while true; do
 
 	## Browse Avahi discover results and format them into a hosts file
-	avahi-browse -f rtp_http._tcp | grep "=" > /tmp/avahi.browse
-	grep \= /tmp/avahi-browse | \
+	avahi-browse -p -r -t -f $PROTOCOLL  | grep -e "=" > $AVAHI_CACHE_FILE
+	grep \= $AVAHI_CACHE_FILE | \
 		sed -e 's|\.local||g' \
 		   -e 's|_|\.|g' | \
-		awk 'BEGIN { FS=";" } { print $8 " " $7; }' > /tmp/avahi.browse.hosts
+		awk 'BEGIN { FS=";" } { print $8 " " $7; }' > $AVAHI_GEN_OUTPUT
 
 
-	echo "# Updated " `date` >> /tmp/avahi.browse.hosts
-	cp /tmp/avahi.browse.hosts $PIRATEBOX_HOSTS_MESH
+	echo "# Updated " `date` >> $AVAHI_GEN_OUTPUT
+	cp $AVAHI_GEN_OUTPUT   $PIRATEBOX_HOSTS_MESH
 
 	# Send SIGHUP to dnsmasq for refreshing its cache
 	kill -1 `cat $DNSMASQ_PID`
