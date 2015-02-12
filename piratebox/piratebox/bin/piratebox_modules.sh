333158912b17cd5bfa27ed3e68d8e3efc3fdcd24
Matthias Strubel <matthias.strubel@aod-rpg.de>
2015-02-12 13:22:34 +0100

    first bulk of overwork before testing

---
+++ piratebox/piratebox/bin/piratebox_modules.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+PIRATEBOX_FOLDER=${PIRATEBOX_FOLDER:=/opt/piratebox}
+PIRATEBOX_CONF="${PIRATEBOX_FOLDER}/conf/piratebox.conf"
+
+. "${PIRATEBOX_CONF}"
+. "${PIRATEBOX_FOLDER}/lib/module_maintain.sh"
+
+DEBUG=${DEBUG:=false}
+
+export cfg_modules=${MODULE_ENABLED}
+export cfg_modules_lib=${MODULE_AVAILABLE}
+
+case "$1" in
+  start)
+	_run_ "start"
+    ;;
+  stop)
+  	_run_ "stop"
+   ;;
+  enable)
+  	_enable_ "$2"
+   ;;   
+  disable)
+  	_disable_ "$2"
+   ;;      
+#  module)
+#  ;;
+  *)
+    echo "Usage: piratebox_modules.sh {start|stop}"
+    echo "       piratebox_modules.sh enable module"
+    echo "       piratebox_modules.sh disable module"
+    exit 1
+    ;;
+esac
+
+exit 0 
---
+++ piratebox/piratebox/lib/module_maintain.sh
@@ -0,0 +1,160 @@
+#!/bin/sh
+
+MODULE_LIST=""
+
+# Opmode Prefixes
+PREFIX_START="S"
+PREFIX_STOP="K"
+
+_get_prefix_for_start(){
+	echo $PREFIX_START
+}
+_get_prefix_for_stop(){
+	echo $PREFIX_STOP
+}
+
+
+_work_on_module_(){
+	local op_mode="$1"
+	local module="$2"
+
+	abort_start=0
+	echo "Working on $module"
+	_load_configuration_ "$module"
+        return "func_${module}_${op_mode}"
+	
+}
+
+#Processes all modules.enabled
+auto_process_all() {
+	local op_mode="$1"
+
+	clean_exit=1
+	abort_start=0
+
+        for module in  $MODULE_LIST
+        do
+		_work_on_module_  "${op_mode}" "${module}"
+		[[ "${abort_start}" == "1" ]] && return 99
+        done
+
+	return 0
+}
+
+_load_configuration_(){
+	local module_name="$1"
+
+	$DEBUG && echo "Loading configuration for ${module_name}"
+	local config_list=$( func_"${module_name}"_get_config ) 
+	$DEBUG && echo "  ... $config_list"
+	
+	for config_file in "$config_list" ; do
+		local is_module_config="yes"
+		if  "$config_file" | grep -q "/" ; then
+			$DEBUG && echo "  .. absolude config path."
+			. $config_file
+		else
+			$DEBUG && echo  "  .. module config."
+			. "${MODULE_CONFIG}/${config_file}"
+		fi
+	done
+	return 0
+}
+
+_load_modules_() {
+	local module_path=$1 ; shift
+
+	local search_prefix=$( _get_prefix_for_$op_mode )
+	local available_module_files=$(ls -x ${cfg_modules}/$[search_prefix}??_*)
+
+	 $DEBUG  && echo "modules_folder $cfg_modules "
+	 $DEBUG  && echo "ls result: $available_module_files"
+
+	
+
+	for this_module in $available_module_files
+	do
+		this_module_name=$( echo "$this_module" | cut -d '_' -f2 )
+		echo -n  "Loading module $this_module_name .."
+		. $cfg_modules/$this_module_name
+		echo "done"
+	done
+
+	echo "Available modules: $MODULE_LIST "
+
+	return 0
+}
+
+_run_single_(){
+	local module_name="$1"
+	local op_mode="$2"
+	
+	 $DEBUG  && echo "Loading Module $module_name"
+	. $cfg_modules/$module_name
+	 $DEBUG  && echo ".. Processing $module_name $op_mode"
+	exit _work_on_module_ "$op_mode" "$module_name"
+}
+
+_run_() {
+	local op_mode="$1"
+
+	clean_exit=1
+
+	# check if $cfg_modules is available
+	if [ ! -d $cfg_modules ] ; then
+		echo "config module folder $cfg_modules does not exists"
+		exit 1
+	fi
+
+	# load modules
+	_load_modules_  "$op_mode" || exit $?
+
+	# Run configuration
+	auto_process_all "$op_mode" 
+
+	
+	if [ "$clean_exit" == "1" ] ; then
+		#If we changed something we deliver 0 as RC, because
+		#we did our work successfully
+		exit 0
+	else
+		#So, we haven't changed something, so, we inform with 
+		#RC 1 as a hint
+		exit 255
+	fi
+}	
+
+_enable_(){
+	local module_name="$1"
+	
+	# check if $cfg_modules is available
+	if [ ! -d $cfg_modules ] ; then
+		echo "config module folder $cfg_modules does not exists"
+		exit 1
+	fi
+	
+	if [ ! -e $cfg_modules_lib/$module_name ] ; then
+		echo "unknown module ${module_name}"
+		exit 1
+	fi
+	
+	 $DEBUG  && echo "Loading Module $module_name"
+	 
+	 local start_num=$( func_"${module_name"}_get_start_order )
+	 local stop_num=$( func_"${module_name"}_get_stop_order )
+	 
+	 local linkname_start="${PREFIX_START}${start_num}_${module_name}"
+	 local linkname_stop="${PREFIX_STOP}${stop_num}_${module_name}"
+
+	cd $cfg_modules
+		#TODO better way to make this relative linking working...
+	ln -s "../modules.available/"$module_name   ${linkname_start}
+	ln -s "../modules.available/"$module_name   ${linkname_stop}
+}
+
+_disable_(){
+	local module_name="$1"
+	cd $cfg_modules
+	ls -1 ./???_$module_name  | xargs -I {} rm -v  {}
+
+}
