001a641309e16d54c0adf9a7673c0aef063564e8
nightshade <nightshade@thebash.ninja>
2015-09-14 17:31:00 -0600

    Add Diskusage to Piratebox

---
+++ piratebox/piratebox/www/cgi-bin/diskusage.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+
+# Modificated ShoutBox Library
+#   enables further modifications for the ShoutBox
+#   Run without to generate htmlfile
+#   Run the following to enter a new line from command line
+#     psogen.py input Anonymous default "Text"
+
+import os, re
+from psutil import disk_usage
+
+htmlfilename = os.environ["DISK_GEN_HTMLFILE"]
+
+#--------------
+#  Generates Shoutbox-HTML-Frame  ...
+#           Imports:
+#               content    -   String  containing preformatted data
+#--------------
+def generate_html(content):
+    htmlstring =   "<html><head><meta http-equiv='cache-control' content='no-cache'><meta name='GENERATOR' content='PyShoutOut'><title>DiskUsage Data</title><body>"
+    htmlstring +=  content
+    htmlstring +=  "</body></html>"
+    return htmlstring
+
+#--------------
+#   Generates HTML Data based on given content  and write it to static html file
+#          Imports:
+#               content    -   String  containing preformatted data
+#--------------
+def generate_html_into_file(content):
+    open(htmlfilename, 'w').close()
+    htmlstring = generate_html ( content )
+    htmlfile = open( htmlfilename , 'w' )
+    htmlfile.truncate()
+    htmlfile.write( htmlstring )
+    htmlfile.close()
+
+#--------------
+# Function for saving the disk usage to a file. Called by HTML-Form
+#--------------
+def get_usage(drive):
+    content = prepare_line( drive )
+    generate_html_into_file ( content )
+
+#--------------
+# Function for returning the amount of free space as an Integer
+#--------------
+def FreeSpace(drive):
+    """ Return the FreeSape of a shared drive in bytes"""
+    try:
+        usage = disk_usage(drive)
+        floatpercent = float(usage.used) / float(usage.total)
+        percent = int(100*floatpercent)
+        return percent
+    except:
+        return 0
+
+
+#--------------
+# Function which formats the about of freespace into a nice readable format
+#--------------
+def prepare_line (drive):
+    data = str(FreeSpace(drive))
+    # Trying to make it look like this:
+    #<div class='progress'>
+    #  <div class='progress-bar' role='progressbar' aria-valuenow='15' aria-valuemin='0' aria-valuemax='100' style='width: 15%'>
+    #    <span class='sr-only'>15% Full</span>
+    #  </div>
+    #</div>
+    content = "<div class='progress'><div class='progress-bar' role='progressbar' aria-valuenow='" + data + "' aria-valuemin='0' aria-valuemax='100' style='width: " + data + "%'><span class='sr-only'>" + data + "% Full</span></div></div>"
+    return content
