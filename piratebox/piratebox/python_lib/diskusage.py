85ac67a41eac00ea414eea91b522bde1caa9e093
Mike Weaver <nightshade@thebash.ninja>
2015-09-16 22:51:56 -0600

    normalize formatting

--- piratebox/piratebox/python_lib/diskusage.py
+++ piratebox/piratebox/python_lib/diskusage.py
@@ -16,14 +16,14 @@ delay = 60*5 #In seconds
 #               content    -   String  containing preformatted data
 #--------------
 def generate_html(content):
-    htmlstring =   "<html><head><meta http-equiv='cache-control' content='no-cache'><meta name='GENERATOR' content='disk usage'><title>DiskUsage Data</title><body>"
-    htmlstring +=  content
-    htmlstring +=  "</body></html>"
-    return htmlstring
+	htmlstring =   "<html><head><meta http-equiv='cache-control' content='no-cache'><meta name='GENERATOR' content='disk usage'><title>DiskUsage Data</title><body>"
+	htmlstring +=  content
+	htmlstring +=  "</body></html>"
+	return htmlstring
 
 def modification_date(filename):
-    moddate = os.path.getmtime(filename)
-    return datetime.datetime.fromtimestamp(moddate)
+	moddate = os.path.getmtime(filename)
+	return datetime.datetime.fromtimestamp(moddate)
 
 #--------------
 #   Generates HTML Data based on given content  and write it to static html file
@@ -31,12 +31,12 @@ def modification_date(filename):
 #               content    -   String  containing preformatted data
 #--------------
 def generate_html_into_file(content):
-    open(htmlfilename, 'w').close()
-    htmlstring = generate_html ( content )
-    htmlfile = open( htmlfilename , 'w' )
-    htmlfile.truncate()
-    htmlfile.write( htmlstring )
-    htmlfile.close()
+	open(htmlfilename, 'w').close()
+	htmlstring = generate_html ( content )
+	htmlfile = open( htmlfilename , 'w' )
+	htmlfile.truncate()
+	htmlfile.write( htmlstring )
+	htmlfile.close()
 
 #--------------
 # Function for saving the disk usage to a file. Called by HTML-Form
@@ -60,41 +60,41 @@ def get_usage(drive):
 # Function for returning the amount of free space as an Integer
 #--------------
 def FreeSpace(drive):
-    """ Return the FreeSape of a shared drive in bytes"""
-    try:
-        usage = disk_usage(drive)
-        floatpercent = float(usage.used) / float(usage.total)
-        percent = int(100*floatpercent)
-        return percent
-    except:
-        return 0
+	""" Return the FreeSape of a shared drive in bytes"""
+	try:
+		usage = disk_usage(drive)
+		floatpercent = float(usage.used) / float(usage.total)
+		percent = int(100*floatpercent)
+		return percent
+	except:
+		return 0
 
 
 #--------------
 # Function which formats the about of freespace into a nice readable format
 #--------------
 def prepare_line (drive):
-    data = str(FreeSpace(drive))
-    # Trying to make it look like this:
-    #<div class='progress'>
-    #  <div class='progress-bar' role='progressbar' aria-valuenow='15' aria-valuemin='0' aria-valuemax='100' style='width: 15%'>
-    #    <span class='sr-only'>15% Full</span>
-    #  </div>
-    #</div>
-    content = "<div class='progress'><div class='progress-bar' role='progressbar' aria-valuenow='" + data + "' aria-valuemin='0' aria-valuemax='100' style='width: " + data + "%'><span class='sr-only'>" + data + "% Full</span></div></div>"
-    return content
+	data = str(FreeSpace(drive))
+	# Trying to make it look like this:
+	#<div class='progress'>
+	#  <div class='progress-bar' role='progressbar' aria-valuenow='15' aria-valuemin='0' aria-valuemax='100' style='width: 15%'>
+	#    <span class='sr-only'>15% Full</span>
+	#  </div>
+	#</div>
+	content = "<div class='progress'><div class='progress-bar' role='progressbar' aria-valuenow='" + data + "' aria-valuemin='0' aria-valuemax='100' style='width: " + data + "%'><span class='sr-only'>" + data + "% Full</span></div></div>"
+	return content
 
 #--------------
 #  Generating static HTML File
 #--------------
 if __name__ == "__main__":
 
-  disk = "/opt/piratebox/share/Shared"
+	disk = "/opt/piratebox/share/Shared"
 
-  if os.path.exists(htmlfilename) != True:
-    content = prepare_line(disk)
-    generate_html_into_file(content)
-  else:
-    get_usage(disk)
+	if os.path.exists(htmlfilename) != True:
+		content = prepare_line(disk)
+		generate_html_into_file(content)
+	else:
+		get_usage(disk)
 
-  print "Generated HTML-DiskUsage File."
+	print "Generated HTML-DiskUsage File."
